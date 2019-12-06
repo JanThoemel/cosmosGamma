f7bead267d72634e2b831f87420e1bbf401b6fa1
Rod <37410820+rodweber@users.noreply.github.com>
2019-12-06 14:01:39 +0100

    added .fetchthis.sh

---
+++ .fetchthis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+printf ">\n>\n> ${PWD##*/}: " && echo "$(PWD)" && git fetch --all && git status && echo "merged:" && git branch -av --merged && echo "not merged:" && git branch -av --no-merged && printf "\n"
