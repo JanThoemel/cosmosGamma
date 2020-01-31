cbc9b6406adbbb9c3009f0197f4df760dfe0bba2
Rod <37410820+rodweber@users.noreply.github.com>
2020-01-31 16:37:56 +0100

    Use proxy service of the PlantUML server to open UML diagram

--- .fetch-this.sh
+++ .fetch-this.sh
@@ -2,10 +2,12 @@
 
 printf ">\n>\n> ${PWD##*/}: " &&
 echo "$(PWD)" &&
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - &&
 git fetch --all &&
 git status &&
 echo "merged:" &&
 git branch -av --merged &&
-echo "not merged:" &&
+echo "non-merged:" &&
 git branch -av --no-merged &&
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - &&
 printf "\n"
