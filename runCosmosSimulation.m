4572d73f880eee442a99e54fae34a8470f012051
rodunilu <60268629+rodunilu@users.noreply.github.com>
2020-07-21 15:13:51 +0200

    Add equatorial orbit that follows Nadir

--- runCosmosSimulation.m
+++ runCosmosSimulation.m
@@ -8,6 +8,7 @@ close all; clear all; clc; %#ok<CLALL>
 
 % Inform the name of this file without the extension "m".
 THIS_FILE_NAME = 'runCosmosSimulation';
+%THIS_FILE_NAME = 'opensim';
 
 % Inform the name of the project file for the Cosmos simulation.
 PROJECT_FILE_NAME = 'cosmos-simulation'; % Without extension 'prj'.
@@ -52,6 +53,53 @@ else
         fprintf('Project path: %s\n',proj.RootFolder);
     end
 end
+
+coord = readmatrix('coordinates');
+lat = coord(:,1); % radians
+long = coord(:,2); % radians
+
+% Check length of lat and long here.
+
+% Get each pair of lat/long.
+
+    % Set original vector1 as [SMA, 0, 0].
+    
+    % Correct orientation of the 3D satellite.
+    % Rotate 90deg (pi/2) on ECEF's Y-axis.
+    
+    % Rotate on ECEF's Y-axis with angle -latitude.
+    % Output vector2.
+    
+    % Rotate on ECEF's Z-axis with angle +longitude.
+    % Output vector3.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    % Normalize vector1 to values between 0 and 1.
+    
+    % Rotate on ECEF's Z-axis with angle = longitude.
+    % Outputs rotated vector2.
+    
+    % Compute ortogonal vector between vector2 and Z-axis positive.
+    % Ortogonal vector must be the rotation axis for vector2 to meet Z+.
+    % Check it.
+    
+    % Compute rotation matrix on ortogonal vector with angle = latitude.
+    % Use Rodrigues' rotation equations.
+    
+    % Use rotation matrix on vector2.
+    % Outputs vector3.
+    
+    
+
+    % Denormalize vector to values between 0 and SMA.
+    
 
 %simulationTime = 20e3; % before: 200e3
 %sim('asbCubeSat',simulationTime);
