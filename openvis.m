7faa7fca962e1c4d2060e84b3b40d148771b3a7b
rodunilu <60268629+rodunilu@users.noreply.github.com>
2020-07-28 12:07:18 +0200

    Implement blocks to get input for visualization of three satellites

--- openvis.m
+++ openvis.m
@@ -16,8 +16,8 @@ PROJECT_FILE_NAME = 'cosmosVisualization'; % Without extension 'prj'.
 PROJECT_FOLDER = 'visualization';
 
 % Files with coordinates data for satellites.
-coordfiles = {'sat1LLR'};
-%coordfiles = {'sat1LLR','sat2LLR','sat3LLR'};
+%coordfiles = {'sat1LLR'};
+coordfiles = {'sat1LLR','sat2LLR','sat3LLR'};
 %coordfiles = {'sat1_LLR_PYR','sat2_LLR_PYR','sat3_LLR_PYR'};
 
 % Inclination for the satellite orbits, in radians.
